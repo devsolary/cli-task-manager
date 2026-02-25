37c3b59c50a682021d6372d14f26f75cf545ad54
devSolary <mathewsolomon91@gmail.com>
2026-02-25 16:02:11 -0600

    add task class

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include "task/task.h"
 using namespace std;
 
 int main() {
--- src/task/task.cpp
+++ src/task/task.cpp
@@ -1,14 +1,30 @@
 #include <iostream>
 #include <string>
 #include <sstream>
+#include "task.h"
 using namespace std;
 
-class task{
-    private:
-        int id;
-        string title, status, startDate, endDate, timeline;
-    public:
-        void setTask(string t, string s, string sd, string ed, string tl){
-            
-        };
-} task;
+class Task
+{
+private:
+    int id;
+    string title, status, startDate, endDate, timeline;
+
+public:
+    void setTask(string t, string s, string sd, string ed, string tl)
+    {
+        title = t;
+        status = s;
+        startDate = sd;
+        endDate = ed;
+        timeline = tl;
+    };
+    string taskInfo()
+    {
+        stringstream ss;
+        ss << title << " | " << status << " | "
+           << startDate << " | " << endDate << " | "
+           << timeline;
+        return ss.str();
+    }
+};
