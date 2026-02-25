120d034f062d8d287e9a17d2e5436f552ee6b1f3
devSolary <mathewsolomon91@gmail.com>
2026-02-25 13:59:57 -0600

    structure file

---
+++ src/task/task.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+using namespace std;
+
+class task{
+    private:
+        int id;
+        string title, status, startDate, endDate, timeline;
+    public:
+        void setTask(string t, string s, string sd, string ed, string tl){
+            
+        };
+} task;
